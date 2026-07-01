be277214a05f0a841e0dc5a8d6adf57736bb6202
Pranima <pranimabhattarai@gmail.com>
2026-07-01 14:00:59 +0545

    Updated popup task

--- task1/task1.js
+++ task1/task1.js
@@ -1,37 +1,59 @@
+// select elements
 
-const timer = document.getElementsByClassName("timer");
-let timesss = null;
-let startTime = 0;
-let elapsedTime = 0;
-let isRunning = false;
+const open = document.querySelector(".open")
+const popup = document.querySelector(".popup")
+const close = document.querySelector(".close")
+const yesBtn = document.querySelector(".yes")
+const noBtn = document.querySelector(".no")
 
-function start(){
-    if(!isRunning){
-        startTime = Date.now() - elapsedTime;
-        timer = setInterval(update,10);
-    }
-}
+open.addEventListener("click", function(){
+    popup.style.display = "flex";
+    open.style.display = "none";
+});
 
-function stop(){
+close.addEventListener("click", function(){
+    popup.style.display = "none";
+    open.style.display = "block";
+});
 
-}
+yesBtn.addEventListener("click", function(){
+    popup.style.display = "none";
+    open.style.display = "block";
+});
 
-function pause(){
+noBtn.addEventListener("click", function(){
+    popup.style.display = "none";
+    open.style.display = "block";
+});
 
-}
 
-function resume(){
-    
-}
+// Selecting the elements
+// const openBtn = document.querySelector(".open");
+// const popup = document.querySelector(".popup");
+// const closeBtn = document.querySelector(".close");
+// const yesBtn = document.querySelector(".yes");
+// const noBtn = document.querySelector(".no");
 
-function update(){
-    const currentTime = Date.now();
-    elapsedTime = currentTime - startTime;
+// Open the popup
+// openBtn.addEventListener("click", function () {
+//     popup.style.display = "flex";
+//     openBtn.style.display = "none";
+// });
 
-    let hours = Math.floor(elapsedTime/(1000*60*60));
-    let minutes = Math.floor(elapsedTime / (1000*60)%60);
-    let seconds = Math.floor(elapsedTime/1000 % 60);
-    let ms = Math.floor(elapsedTime % 1000 / 10);
+// // Close popup when X is clicked
+// closeBtn.addEventListener("click", function () {
+//     popup.style.display = "none";
+//     openBtn.style.display = "block";
+// });
 
-    display.textContent = ``
-}
+// // Close popup when Yes is clicked
+// yesBtn.addEventListener("click", function () {
+//     popup.style.display = "none";
+//     openBtn.style.display = "block";
+// });
+
+// // Close popup when No is clicked
+// noBtn.addEventListener("click", function () {
+//     popup.style.display = "none";
+//     openBtn.style.display = "block";
+// });
