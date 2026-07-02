cd1680edf7ebea123412302e09c2d2604ee07eb0
Pranima <pranimabhattarai@gmail.com>
2026-07-02 23:00:15 +0545

    made sidebar entirely

---
+++ sidebar/sidebar.js
@@ -0,0 +1,17 @@
+const sideBtn = document.querySelector(".sideBtn")
+const sidebar = document.querySelector(".sidebar")
+const sideBtn1 = document.querySelector("sideBtn1")
+const sidebarS = document.querySelector(".sidebarS")
+
+
+sideBtn.addEventListener("click",function(){
+    sidebar.style.display = "none"
+    sidebarS.style.display = "flex"
+})
+
+sideBtn1.addEventListener("click",function(){
+    sidebarS.style.display = "none"
+    sidebar.style.display = "flex"
+})
+
+
--- todo/todo.js
+++ todo/todo.js
@@ -1,6 +1,6 @@
 const addBtn = document.querySelector(".add")
-const cards = document.querySelector(".cards")
-const searchBar = document.querySelector(".search")
+const cards = document.querySelector("cards")
+const searchBar = document.querySelector("search")
 const addPage
 
 addBtn.addEventListener("click", function(){
