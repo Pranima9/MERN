00695e8d04a897e0add29d440dbd9fcd06bdb7ec
Pranima <pranimabhattarai9@gmail.com>
2026-07-27 12:30:43 +0545

    crud routes

--- BACKEND/routes/user.route.js
+++ BACKEND/routes/user.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import {getUser, postUser} from "../controllers/user.js"
+import {deleteUser, editUser, getSingleUser, getUser, postUser} from "../controllers/user.js"
 
 const route = express.Router();
 
 route.get("/get-data", getUser) //imported from controllers/user.js
 route.post("/post-data", postUser) 
+route.get("/user/:id", getSingleUser)
+route.delete("user/:id", deleteUser)
+route.patch("user/:id",editUser)
 
 export default route;
