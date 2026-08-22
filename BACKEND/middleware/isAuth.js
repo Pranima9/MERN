86780ee37df795bd154d5d2c5253569c2847a1dc
Pranima <pranimabhattarai9@gmail.com>
2026-08-22 15:39:57 +0545

    Authentication userrole

--- BACKEND/middleware/isAuth.js
+++ BACKEND/middleware/isAuth.js
@@ -1,3 +1,30 @@
+import jwt from "jsonwebtoken";
+
 export const isLoggedIn = (req, res, next) => {
-    const token = req.header.authorization;
+    const token = req.headers?.authorization?.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).send({message: "Please Login First..."});
+    }
+
+    
+  const verify = jwt.verify(token, "secretkey");
+  console.log(verify);
+
+  req.userRole =
+    verify.role === 1
+      ? "Admin"
+      : verify.role === 2
+        ? "Manager"
+        : verify.role === 3
+          ? "Staff"
+          : verify.role === 4
+            ? "User"
+            : null;
+
+  next();
+
 };
+
+
+// complete
